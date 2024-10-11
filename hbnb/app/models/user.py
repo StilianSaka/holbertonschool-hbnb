8ae6c89717e2fc387ce2748847d48c133b9e3529
Flavio Avdulla <a.flavio4366@gmail.com>
2024-10-11 09:39:00 +0200

    user_class

--- hbnb/app/models/user.py
+++ hbnb/app/models/user.py
@@ -8,35 +8,42 @@ class User:
         self.password = password
         self.is_admin = is_admin
 
-	def register(self):
-		return f"The user {self.name} {self.last_name} is registered."
-        
-    def update(self, first_name=None, last_name=None, email=None, password=None):
+        def register_user(self):
+            # Registration logic here
+        return f"User {self.full_name()} registered."
 
-    	if first_name:
-    		self.first_name = first_name
+    def update_user_info(self, first_name=None, last_name=None, email=None, password=None):
+
+        if first_name:
+            self.first_name = first_name
 
         if last_name:
-			self.last_name = last_name
+            self.last_name = last_name
 
-		if email:
-			self.email = email
-		
-		if password:
-			self.password = password
-		
-		if is_admin
-			self.is_admin = is_admin
-		return f"User {self.first_name} {self.last_name} is Updated"
-		
-		
+        if email:
+            self.email = email
 
-	def delete_user(self):
-		return f"The user {self.first_name} {self.last_name} is deleted."
+        if password:
+            self.password = password
 
-	def list_user_info(self):
-		return f"The user info is: {self.first_name}, {self.last_name}, {self.email}, {self.password}"
+        if is_admin is not None:
+            self.is_admin = is_admin
+        return f"The User {self.full_name()} updated."
 
-	def full_name(self):
-		return f" The user fullname is: {self.first_name}, {self.last_name}"
-	
+        def delete_user(self):
+            return f"The user {self.first_name} {self.last_name} is deleted."
+
+        def list_user_info(self):
+            return f"The user info is: {self.first_name}, {self.last_name}, {self.email}, {self.password}"
+
+        def full_name(self):
+            return f" The user fullname is: {self.first_name}, {self.last_name}"
+
+
+if __name__ == "__main__":
+    user = User("Jane", "Doe", "jane.doe@example.com", "securepassword", False)
+    print(user.register())
+    print(user.list())
+    print(user.update_user_info(first_name="Janet", is_admin=True))
+    print(user.list_user_info())
+    print(user.delete_user())
